163045c4060abf5c0e1e01d4d2c3a657152b759a
rushikesh109 <139239226+rushikesh109@users.noreply.github.com>
2024-02-06 02:00:18 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,37 +1,56 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const mongoDBSession = require('connect-mongodb-session')(session)
+const mongoURI = "mongodb://localhost:27017/pizza"
+
+
+// Database connection
+mongoose.connect(mongoURI, {})
+    .then(() => console.log("Database connected..."))
+    .catch((err) => console.log(err));
+
+const mongoStore = new mongoDBSession({
+    uri: mongoURI,
+    collection: 'sessions'
+})
+
+
+// session config
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: mongoStore,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hrs
+}))
+
+app.use(flash())
 
 // assets 
 app.use(express.static('public'))
+app.use(express.json())
 
-
+// Global middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
 
 //set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-
-app.get('/', (req, res) =>{
-    res.render('home')
-})
-
-app.get('/cart' , (req, res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login' , (req, res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register' , (req, res)=>{
-    res.render('auth/register')
-})
-
+require('./routes/web')(app)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
