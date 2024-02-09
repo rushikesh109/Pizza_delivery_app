859f1b5ab3a59391fdb438392cf49d93a01303c6
rushikesh109 <139239226+rushikesh109@users.noreply.github.com>
2024-02-10 03:12:58 +0530

    auth system

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const session = require('express-session')
 const flash = require('express-flash')
 const mongoDBSession = require('connect-mongodb-session')(session)
 const mongoURI = "mongodb://localhost:27017/pizza"
+const passport = require('passport')
 
 
 // Database connection
@@ -22,26 +23,36 @@ const mongoStore = new mongoDBSession({
     collection: 'sessions'
 })
 
-
 // session config
 
-app.use(session({
+app.use(require('express-session')({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: true,
     store: mongoStore,
+    store: mongoStore,
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hrs
 }))
+
+
+//passport config
+
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+// app.use(passport.session())
 
 app.use(flash())
 
 // assets 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
